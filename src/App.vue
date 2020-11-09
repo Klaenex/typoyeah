ea2a22b8009d8442c08c8819a788433b8b8575b6
Vincenzo Cuozzo <klaenex@MBP-de-Vincenzo.lan>
2020-11-09 18:02:31 +0100

    avancement

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,85 @@
 <template>
-    <div class="typo">
+  <div class="typo">
     <input
       type="text"
       name="search"
       id="search"
       class="search"
-      placeholder="Prénom"
+      placeholder="Essaye moi"
       v-model="keyword"
     />
-    <hr />
+
     <ul class="grid">
-      <li v-for="font in people" :key="font.letter">
-        <img :src="font.img" :alt="font.letter" />
+      <li class="list" v-for="font in keyword" :key="font" :class="{ font }">
+        <div>{{ keyword }}</div>
+        <div>{{ filtered }}</div>
+        <div>{{ tab.value }}</div>
+        <div>{{table}}</div>
+
+        <!-- <img class="img_list" :src="require( filtered )"/>    -->
+        <!-- <img
+          class="img_list"
+          :src="'img/' + filtered + '.png'"
+          :alt="font.letter"
+        /> -->
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { defineComponent, computed, ref } from 'vue'
-import data from '@/inc/data'
+import { defineComponent, ref, computed } from 'vue'
 
+//import data from '@/inc/data'
 export default defineComponent({
   name: 'typo',
   setup() {
     const keyword = ref('')
-    const filtered= computed(()=>
-      data.typo.filter(p=> 
-      p.letter.toLowerCase().includes(keyword.value.toLowerCase())
+    const str = keyword.value.substr(keyword.value.length - 1)
 
-     )
+    console.log(keyword.value)
+    console.log(typeof keyword.value)
+    console.log(str)
+    let table=[];
+    const tab=computed(
+      ()=>
+      table.push(keyword.value.substr(keyword.value.length - 1))
+    );
+
+    const filtered = computed(
+      () => 
+      keyword.value.substr(keyword.value.length - 1)
+      
+
+      //   // data.typo.filter(p=> p.letter.toLowerCase().includes(keyword.value.toLowerCase()))
+
+      //   //data.typo.letter=='A'
+      //   //data.typo.filter(f => f.letter.toLowerCase()==keyword.value.toLowerCase()),
     )
+
     return {
-      people: filtered,
-      keyword
+      keyword,
+      str,
+      filtered,
+      table,
+      tab
     }
   },
 })
 </script>
 
 <style lang="scss">
-
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  list-style-type: none;
+}
+.list {
+  width: 200px;
+  height: auto;
+}
+.img_list {
+  width: 100%;
+}
 </style>
